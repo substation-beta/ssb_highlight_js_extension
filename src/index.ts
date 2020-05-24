2a3f558026a859ecaf6564bca688001db51100e0
bastianganze <bganze@foxbase.de>
2020-05-24 12:01:39 +0200

    Adjusted colors and broke up sections for separate usage

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,17 @@
 declare const hljs;
-import { SSBHlJS } from "./ssb.highlight";
+import {
+  SSBHlJS,
+  SSBHlJS_INFO_SECTION,
+  SSBHlJS_TARGET_SECTION,
+  SSBHlJS_EVENTS_SECTION,
+  SSBHlJS_MACROS_SECTION,
+  SSBHlJS_RESOURCES_SECTION
+} from "./ssb.highlight";
 
 hljs.registerLanguage('ssb', SSBHlJS);
+hljs.registerLanguage('ssb-info-section', SSBHlJS_INFO_SECTION);
+hljs.registerLanguage('ssb-target-section', SSBHlJS_TARGET_SECTION);
+hljs.registerLanguage('ssb-events-section', SSBHlJS_EVENTS_SECTION);
+hljs.registerLanguage('ssb-macros-section', SSBHlJS_MACROS_SECTION);
+hljs.registerLanguage('ssb-resources-section', SSBHlJS_RESOURCES_SECTION);
 hljs.initHighlightingOnLoad();
--- src/ssb.highlight.ts
+++ src/ssb.highlight.ts
@@ -1,16 +1,16 @@
 const ATTRIBUTES = {
   begin: '(.*?):',
   end: '$',
-  className: 'attribute',
+  className: 'ssb-attribute',
   returnBegin: true,
   excludeEnd: true,
   contains: [
     {
       begin: '(.*?):',
-      className: 'attribute-name'
+      className: 'ssb-attribute-name'
     },
     {
-      className: 'attribute-value',
+      className: 'ssb-attribute-value',
       endsWithParent: true
     }
   ]
@@ -19,16 +19,16 @@ const ATTRIBUTES = {
 const MACRO_INSERT = {
   begin: '\\${',
   end: '}',
-  className: 'macro-insert'
+  className: 'ssb-macro-insert'
 };
 
 const EVENT_LINE = {
   endsWithParent: true,
-  className: 'event-line',
+  className: 'ssb-event-line',
   contains: [{
     begin: '\\[',
     end: '\\]',
-    className: 'event',
+    className: 'ssb-event',
     contains: [
       MACRO_INSERT
     ]
@@ -39,8 +39,102 @@ const EVENT_LINE = {
 
 const COMMENT_LINE = {
   begin: '//(.*)$',
-  className: 'comment'
+  className: 'ssb-comment'
 };
+
+const INFO_SECTION_RULES = [
+    COMMENT_LINE,
+    {
+      begin: '#INFO',
+      className: 'ssb-section-title'
+    },
+    ATTRIBUTES
+  ];
+
+const TARGET_SECTION_RULES = [
+    COMMENT_LINE,
+    {
+      begin: '#TARGET',
+      className: 'ssb-section-title'
+    },
+    ATTRIBUTES
+  ];
+
+const MACROS_SECTION_RULES = [
+    COMMENT_LINE,
+    {
+      begin: '#MACROS',
+      className: 'ssb-section-title'
+    },
+    {
+      begin: '(.*?):',
+      end: '$',
+      className: 'ssb-attribute',
+      returnBegin: true,
+      excludeEnd: true,
+      contains: [
+        {
+          begin: '^(.*?):',
+          className: 'ssb-macro-name'
+        },
+        // this matches everything not matched yet but would match everything until the end of the document, so we need endsWithParent:true
+        {
+          className: 'ssb-macro-value',
+          endsWithParent: true,
+          contains: [EVENT_LINE]
+        }
+      ]
+    }
+  ];
+
+const EVENTS_SECTION_RULES = [
+    COMMENT_LINE,
+    {
+      begin: '#EVENTS',
+      className: 'ssb-section-title'
+    },
+    {
+      begin: '^[^/][^/](.*?)[|](.*?)[|](.*?)[|](.*)',
+      end: '$',
+      className: 'ssb-event',
+      returnBegin: true,
+      returnEnd: true,
+      contains: [
+        {
+          begin: '^(.*?)(?=[|](.*?)[|](.*?)[|](.*))',
+          className: 'ssb-event-timestamp'
+        },
+        {
+          begin: '[|]',
+          className: 'ssb-event-separator'
+        },
+        {
+          begin: '(.*?)(?=[|](.*?)[|](.*))',
+          className: 'ssb-event-macro'
+        },
+        {
+          begin: '(.*?)(?=[|](.*))',
+          className: 'ssb-comment'
+        },
+        {
+          className: 'ssb-event-text',
+          contains: [
+            EVENT_LINE
+          ],
+          endsWithParent: true
+        }
+      ]
+    }
+  ];
+
+const RESOURCES_SECTION_RULES = [
+    COMMENT_LINE,
+    {
+      begin: '#RESOURCES',
+      className: 'ssb-section-title'
+    },
+    ATTRIBUTES
+  ];
 
 export const SSBHlJS = function(hljs) {
   return {
@@ -49,117 +143,68 @@ export const SSBHlJS = function(hljs) {
       {
         begin: '(?=#INFO)',
         end: '(?![^#])',
-        className: 'section',
-        contains: [
-          COMMENT_LINE,
-          {
-            begin: '#INFO',
-            className: 'section-title'
-          },
-          ATTRIBUTES
-        ]
+        className: 'ssb-section',
+        contains: INFO_SECTION_RULES
       },
       {
         begin: '(?=#TARGET)',
         end: '(?![^#])',
-        className: 'section',
-        contains: [
-          COMMENT_LINE,
-          {
-            begin: '#TARGET',
-            className: 'section-title'
-          },
-          ATTRIBUTES
-        ]
+        className: 'ssb-section',
+        contains: TARGET_SECTION_RULES
       },
       {
         begin: '(?=#MACROS)',
         end: '(?![^#])',
-        className: 'section',
-        contains: [
-          COMMENT_LINE,
-          {
-            begin: '#MACROS',
-            className: 'section-title'
-          },
-          {
-            begin: '(.*?):',
-            end: '$',
-            className: 'attribute',
-            returnBegin: true,
-            excludeEnd: true,
-            contains: [
-              {
-                begin: '^(.*?):',
-                className: 'macro-name'
-              },
-              // this matches everything not matched yet but would match everything until the end of the document, so we need endsWithParent:true
-              {
-                className: 'macro-value',
-                endsWithParent: true,
-                contains: [EVENT_LINE]
-              }
-            ]
-          }
-        ]
+        className: 'ssb-section',
+        contains: MACROS_SECTION_RULES
       },
       {
         begin: '(?=#EVENTS)',
         end: '(?![^#])',
-        className: 'section',
-        contains: [
-          COMMENT_LINE,
-          {
-            begin: '#EVENTS',
-            className: 'section-title'
-          },
-          {
-            begin: '^[^/][^/](.*?)[|](.*?)[|](.*?)[|](.*)',
-            end: '$',
-            className: 'event',
-            returnBegin: true,
-            returnEnd: true,
-            contains: [
-              {
-                begin: '^(.*?)(?=[|](.*?)[|](.*?)[|](.*))',
-                className: 'event-timestamp'
-              },
-              {
-                begin: '[|]',
-                className: 'event-separator'
-              },
-              {
-                begin: '(.*?)(?=[|](.*?)[|](.*))',
-                className: 'event-macro'
-              },
-              {
-                begin: '(.*?)(?=[|](.*))',
-                className: 'comment'
-              },
-              {
-                className: 'event-text',
-                contains: [
-                  EVENT_LINE
-                ],
-                endsWithParent: true
-              }
-            ]
-          }
-        ]
+        className: 'ssb-section',
+        contains: EVENTS_SECTION_RULES
       },
       {
         begin: '(?=#RESOURCES)',
         end: '(?![^#])',
-        className: 'section',
-        contains: [
-          COMMENT_LINE,
-          {
-            begin: '#RESOURCES',
-            className: 'section-title'
-          },
-          ATTRIBUTES
-        ]
+        className: 'ssb-section',
+        contains: RESOURCES_SECTION_RULES
       }
     ]
+  };
+};
+
+export const SSBHlJS_INFO_SECTION = function(hljs) {
+  return {
+    aliases: ['ssb-info-section'],
+    contains: INFO_SECTION_RULES
+  };
+};
+
+export const SSBHlJS_TARGET_SECTION = function(hljs) {
+  return {
+    aliases: ['ssb-target-section'],
+    contains: TARGET_SECTION_RULES
+  };
+};
+
+export const SSBHlJS_MACROS_SECTION = function(hljs) {
+  return {
+    aliases: ['ssb-macros-section'],
+    contains: MACROS_SECTION_RULES
+  };
+};
+
+export const SSBHlJS_EVENTS_SECTION = function(hljs) {
+  return {
+    aliases: ['ssb-events-section'],
+    contains: EVENTS_SECTION_RULES
+  };
+};
+
+export const SSBHlJS_RESOURCES_SECTION = function(hljs) {
+  return {
+    aliases: ['ssb-resources-section'],
+    contains: RESOURCES_SECTION_RULES
   };
 };
